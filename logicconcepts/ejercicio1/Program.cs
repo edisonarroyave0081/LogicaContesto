55459812b1c6c7818de11ec03a8a68ef34df260c
edisonarroyave0081 <ediarroyave0081@gmail.com>
2024-03-31 01:40:35 -0500

    segundo comit

---
+++ logicconcepts/ejercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+var numberstring = string.Empty;
+do
+{
+    Console.WriteLine("digite numero es i gual a s sale");
+     numberstring = Console.ReadLine();
+    if (numberstring == "s")
+    {
+        continue;
+
+    }
+
+    var number =0;
+
+ 
+    if (int.TryParse(numberstring, out number)) { 
+    if (number % 2 == 0)
+    {
+        Console.WriteLine("el numero es par");
+
+    }
+    else
+    {
+        Console.WriteLine("el numero no es apr");
+    }
+}
+    else
+    {
+        Console.WriteLine($"lo que ingresaste {number} no es un  numero entero");
+    }
+}while(numberstring!="s");
