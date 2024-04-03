bb3af7a1dc3e4c564ca7fb012c332b49f304d55f
edisonarroyave0081 <ediarroyave0081@gmail.com>
2024-04-02 23:28:52 -0500

    comit3

---
+++ logicconcepts/shared/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared
+{
+    public class Class1
+    {
+        public static int  GetInt(string message) { 
+        Console.WriteLine(message);
+            var numberString=Console.ReadLine();
+            var number = 0;
+            if (int.TryParse(numberString, out number))
+            {
+                return number;
+            }
+            return 0;
+        }
+    }
+}
+ 
